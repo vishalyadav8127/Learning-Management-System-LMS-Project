a65bec25e19f0ef3fcbb3ac535e818650bf2211f
Vishal <vy812794@gmail.com>
2025-01-03 17:32:49 +0530

    Added 404 Not Found Pagen And complete signup flow

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage.jsx';
 import AboutUs from './Pages/AboutUs.jsx';
+import NotFound from './Pages/NotFound.jsx';
+import Signup from './Pages/Signup.jsx';
 
 
 function App() {
@@ -9,9 +11,14 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage/>}>
-        <Route path="/about" element={<AboutUs/>}></Route>\
-        </Route>
+        <Route path="/" element={<HomePage/>}></Route>
+
+        <Route path="/about" element={<AboutUs/>}></Route>
+
+        <Route path="/signup" element={<Signup/>}></Route>
+
+        <Route path="*" element={<NotFound/>}></Route>
+
       </Routes>
       
     </>
